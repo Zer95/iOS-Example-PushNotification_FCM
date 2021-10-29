f48fd0517f011ff4515e78cb8105fd6480ac62f2
Precial <ggrty10@gmail.com>
2021-10-29 18:40:51 +0900

    Insert FirebaseConnect & Remote Notification

--- PushNoti_FCM_Example/AppDelegate.swift
+++ PushNoti_FCM_Example/AppDelegate.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 import Firebase
+import UserNotifications
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+
         FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        
+        //FCM 현재 등록 토큰 확인
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("ERROR FMC 등록토큰 가져오기: \(error.localizedDescription)")
+            } else if let token = token {
+                print("FCM 등록토큰: \(token)")
+            }
+        }
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, error in
+            print("ERROR, Request Notifications Authorization: \(error.debugDescription)")
+        }
+        
+        application.registerForRemoteNotifications()
         return true
     }
 
@@ -28,11 +53,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-       
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .badge, .sound])
+    }
+}
+
+
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let token = fcmToken else {return}
+        print("FCM 등록토큰 갱신: \(token)")
+    }
+}
 
 
